9da7ba83ff19a39637b83a3f7b6f3945c281fb7c
Francis Kogge <koggefa@gmail.com>
2021-11-03 23:50:37 -0700

    So far 1 node can join and start up its server loop. Added some more boilerplate. Still need to look into how to join multiple nodes.

--- chord_node.py
+++ chord_node.py
@@ -2,12 +2,16 @@ import socket
 import threading
 import sys
 import pickle
+import hashlib
+import datetime
 
 M = 3  # FIXME: Test environment, normally = hashlib.sha1().digest_size * 8
 NODES = 2 ** M
 BUF_SZ = 4096  # socket recv arg
 BACKLOG = 100  # socket listen arg
 TEST_BASE = 43544  # for testing use port numbers on localhost at TEST_BASE+n
+POSSIBLE_HOSTS = ('localhost',)
+POSSIBLE_PORTS = range(2 ** 16)
 
 
 class ModRange(object):
@@ -127,15 +131,17 @@ class FingerEntry(object):
 class ChordNode(object):
     def __init__(self, n):
         self.node = n
-        self.finger = [None] + [FingerEntry(n, k) for k in
-                                range(1, M + 1)]  # indexing starts at 1
+        self.finger = [None] + [FingerEntry(n, k) for k in range(1, M + 1)]  # indexing starts at 1
         self.predecessor = None
         self.keys = {}
 
-        self.listener, self.listener_address = self.start_listening_server()
+        self.listener_address = ('localhost', TEST_BASE + n)
+        self.listener = self.start_listening_server()
+        print('I joined!')
 
-    def run(self):
+    def run_server(self):
         while True:
+            print('running forever')
             client_sock, client_address = self.listener.accept()
             threading.Thread(target=self.handle_rpc,
                              args=(client_address,)).start()
@@ -162,13 +168,74 @@ class ChordNode(object):
         np = self.find_predecessor(id)
         return self.call_rpc(np, 'successor')
 
-    @staticmethod
-    def start_listening_server():
+    def start_listening_server(self):
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listener.bind(('localhost', 0))
+        listener.bind(self.listener_address)
         listener.listen(BACKLOG)
-        return listener, listener.getsockname()
+        return listener
 
+    def find_predecessor(self, id):
+        pass
+
+    def call_rpc(self, method_name, n_prime, param):
+        pass
+
+    def join(self, n_prime=None):
+        if n_prime:
+            self.init_finger_table(n_prime)
+            self.update_others()
+        else:
+            for i in range(1, M + 1):
+                self.finger[i].node = self
+            self.predecessor = self
+
+    def update_others(self):
+        """ Update all other node that should have this node in their finger tables """
+        # print('update_others()')
+        for i in range(1,
+                       M + 1):  # find last node p whose i-th finger might be this node
+            # FIXME: bug in paper, have to add the 1 +
+            p = self.find_predecessor((1 + self.node - 2 ** (i - 1) + NODES) % NODES)
+            self.call_rpc(p, 'update_finger_table', self.node, i)
+
+    def update_finger_table(self, s, i):
+        """ if s is i-th finger of n, update this node's finger table with s """
+        # FIXME: don't want e.g. [1, 1) which is the whole circle
+        if (self.finger[i].start != self.finger[i].node
+                # FIXME: bug in paper, [.start
+                and s in ModRange(self.finger[i].start, self.finger[i].node,
+                                  NODES)):
+            print(
+                'update_finger_table({},{}): {}[{}] = {} since {} in [{},{})'.format(
+                    s, i, self.node, i, s, s, self.finger[i].start,
+                    self.finger[i].node))
+            self.finger[i].node = s
+            print('#', self)
+            p = self.predecessor  # get first node preceding myself
+            self.call_rpc(p, 'update_finger_table', s, i)
+            return str(self)
+        else:
+            return 'did nothing {}'.format(self)
+
+    @staticmethod
+    def print_time(date_time):
+        """
+        Printing helper for current timestamp.
+        :param date_time: datetime object
+        """
+        return date_time.strftime('%H:%M:%S.%f')
+
+class Chord(object):
+
+    @staticmethod
+    def lookup_node(address):
+        marshalled_address = pickle.dumps(address)
+        marshalled_hash = hashlib.sha1(marshalled_address).digest()
+        int_hash = int.from_bytes(marshalled_hash, byteorder='big')
+        bits = None
+        if bits is not None:
+            int_hash %= 2 ** bits
+        return int_hash
 
 def main():
     if len(sys.argv) != 2:
@@ -176,10 +243,38 @@ def main():
               'starting new network)')
         exit(1)
 
-    node_port = sys.argv[1]
+    node_port = int(sys.argv[1])
+
+
     if node_port == 0:
-        node = ChordNode()
-        node.run()
+        # Create new Chord node
+        # Start a new network with ID = 0
+        new_node = ChordNode(node_port)
+        new_node.join()
+
+    else:
+        # Lookup node ID from the known Chord node port passed in
+
+        # Hash the address of the KNOWN node port
+        address_hash = hashlib.sha1(pickle.dumps((POSSIBLE_HOSTS, node_port)))
+        # Get node ID of the KNOWN node
+        node_id = Chord.lookup_node(address_hash)
+        # Create a NEW node TODO how to figure out the next ID value to add?
+        new_node = ChordNode(1)
+        # Join the NEW node to the existing network using the KNOWN node ID
+        new_node.join(node_id)
+
+    new_node.run_server()
+
+
+    # if node_port == 0:
+    #     # Start a new network
+    #     node = ChordNode(node_id)
+    #     node.run_server()
+    # else:
+    #     # Join the existing network with known node port number
+    #     # TODO create new node -
+    #     pass
 
 
 
